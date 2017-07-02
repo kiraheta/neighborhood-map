f62134fb6214b07d5934b9969a933378c146fd0c
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-07-02 00:24:34 -0700

    Implement responsive web design

--- js/app.js
+++ js/app.js
@@ -18,10 +18,10 @@ function initMap() {
   // Creates infowindow object for a marker to display info
   var largeInfowindow = new google.maps.InfoWindow();
   // Style the markers a bit. This will be our listing marker icon.
-  var defaultIcon = makeMarkerIcon('0091ff');
+  var defaultIcon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
   // Create a "highlighted location" marker color for when the user
   // mouses over the marker.
-  var highlightedIcon = makeMarkerIcon('FFFF24');
+  var highlightedIcon = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
 
   // Uses the location array to create an array of markers on initialize.
   for (var i = 0; i < locations.length; i++) {
@@ -30,12 +30,12 @@ function initMap() {
     var title = locations[i].title;
     // Create a marker per location, and put into markers array.
     var marker = new google.maps.Marker({
-    map: map,
-    position: position,
-    title: title,
-    animation: google.maps.Animation.DROP,
-    icon: defaultIcon,
-    id: i
+      map: map,
+      position: position,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      icon: defaultIcon,
+      id: i
     });
     // Push the marker to our array of markers.
     markers.push(marker);
@@ -72,7 +72,7 @@ function populateInfoWindow(marker, infowindow) {
   // Check to make sure the infowindow is not already open on this marker.
   if (infowindow.marker != marker) {
     infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>');
+    infowindow.setContent('<div>' + 'Hi! From ' + marker.title + '</div>');
     infowindow.open(map, marker);
     // Make sure the marker property is cleared if the infowindow is closed.
     infowindow.addListener('closeclick',function(){
@@ -101,27 +101,15 @@ function hideListings() {
   }
 }
 
-// This function takes in a COLOR, and then creates a new marker
-// icon of that color. The icon will be 21 px wide by 34 high, have an origin
-// of 0, 0 and be anchored at 10, 34).
-function makeMarkerIcon(markerColor) {
-  var markerImage = new google.maps.MarkerImage(
-    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+
-    markerColor + '|40|_|%E2%80%A2',
-    new google.maps.Size(21, 34),
-    new google.maps.Point(0, 0),
-    new google.maps.Point(10, 34),
-    new google.maps.Size(21,34));
-  return markerImage;
-}
-
 // Handle Google Maps API error
 function googleMapsError() {
-	alert("Google Maps failed to load. Please try again.");
+  alert("Google Maps failed to load. Please try again.");
 }
 
 // Defines the data and behavior of UI
 function AppViewModel() {
+  var self = this;
+  self.locationList = ko.observable(locations);
 }
 
 // Activate knockout.js
